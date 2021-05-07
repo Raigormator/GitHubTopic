793adfb58916da7487654c4a6a232d29ec0b035e
Aaron Kyle Velasquez <avelasquez4@gitlab.com>
2021-05-07 15:40:12 +0800

    add cat logic

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		Dog dog = new Dog();
 		System.out.println(dog.bark());
+
+		Cat cat = new Cat();
+		System.out.println(cat.meow());
 	}
 
-}
+}	
